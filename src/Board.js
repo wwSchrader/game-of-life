4d7a6fe3f56652a64018a730d830ab63024d888d
wwSchrader <wwschrader@gmail.com>
2017-07-05 11:15:57 -0700

    Add cells that change status based on alive or dead status

--- src/Board.js
+++ src/Board.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
+import Cell from './Cell'
 
 export default class Board extends Component {
     constructor(props) {
         super(props);
 
-        this.cellStatus = [[1, 2], [3, 4]]
+        this.cellStatus = [[true, false], [false, true]]
     }
 
     render() {
         var row = this.cellStatus.map(function (item, i) {
             var entry = item.map(function (element, j) {
                 return (
-                    <td key={j}> {element} </td>
+                    <Cell key={i + "" + j} alive={element}/>
                 );
             });
             return (
-                <tr key={i}> {entry} </tr>
+                <tr key={i}>{entry}</tr>
             );
         });
 
         return(
             <table>
-                {row}
+                <tbody>
+                    {row}
+                </tbody>
             </table>
         );
     }
